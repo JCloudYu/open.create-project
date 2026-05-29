f6acf014de075b4b23f94366497396e8864c7c42
JCloudYu <j.cloud.yu@gmail.com>
2026-05-29 11:56:20 +0800

    Rename project and remove redundant project sources

--- project-list.js
+++ project-list.js
@@ -1,13 +1,17 @@
 module.exports = {
+	/*
 	'elmjs-base': {
 		latest: 'https://github.com/JCloudYu/open.proj-tmpl.elmjs-base/archive/refs/heads/master.tar.gz'
 	},
+	*/
 	'fastify-api-base': {
 		latest: 'https://github.com/JCloudYu/open.proj-tmpl.fastify-api-base/archive/refs/heads/master.tar.gz'
 	},
+	/*
 	'fastify-msr': {
 		latest: 'https://github.com/JCloudYu/open.proj-tmpl.fastify-msr/archive/refs/heads/master.tar.gz'
 	},
+	*/
 	'fastvue': {
 		latest: 'https://github.com/JCloudYu/open.proj-tmpl.fastvue/archive/refs/heads/master.tar.gz'
 	},
